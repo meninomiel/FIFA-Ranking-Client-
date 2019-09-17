794759dd71c29945ef9f30c810b31321b35c54d0
Edmiel Leandro <edmiel.leandro@sectec.local>
2019-09-17 17:28:29 -0300

    Add new player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Ranking from './components/ranking/Ranking';
+import Players from './components/players/Players';
 
 function App() {
   return (
@@ -22,46 +24,8 @@ function App() {
       </header>
 
       <section className="content">
-        <table className="ranking">
-          <thead>
-            <tr>
-              <th colSpan="3" className="player-title">Jogador</th>                        
-              <th>Pts</th>
-              <th>VIT</th>
-              <th>E</th>
-              <th>DER</th>
-              <th>GP</th>
-              <th>GC</th>
-              <th>SG</th>
-              <th className="last-five-title">Últimas Cinco</th>
-            </tr>            
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td className="cell-pic">
-                <img className="player-pic"/>
-              </td>
-              <td className="player-name">Edmiel</td>
-              <td>10</td>
-              <td>3</td>
-              <td>1</td>
-              <td>3</td>
-              <td>5</td>
-              <td>2</td>
-              <td>2</td>
-              <td>
-                <div className="last-five">
-                  <img src={require('./images/ranking/win.svg')} alt="vitória" />
-                  <img src={require('./images/ranking/lose.svg')} alt="vitória" />
-                  <img src={require('./images/ranking/draw.svg')} alt="vitória" />
-                  <img src={require('./images/ranking/lose.svg')} alt="vitória" />
-                  <img src={require('./images/ranking/win.svg')} alt="vitória" />
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        {/* <Ranking /> */}
+        <Players />
       </section>
     </main>
     
---
+++ src/components/players/Players.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './Players.css';
+import $ from 'jquery';
+import PubSub from 'pubsub-js';
+
+export default class Players extends Component {
+    constructor(){
+        super();
+        this.state = {
+            posicao: 0,
+            nome: '',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            gPro: 0,
+            gContra: 0,
+            saldo:0,
+            histórico: []
+        }; 
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.sendNewPlayer.bind(this)}>
+                <input type="Text" value={this.state.nome} onChange={this.setNome.bind(this)}/>
+                <input type="submit"/>
+            </form>
+        );
+    }
+
+    sendNewPlayer(sender){
+        sender.preventDefault();
+
+        $.ajax({
+            url:"http://localhost:8080/api/jogadores/",
+            dataType: "json",
+            contentType: "application/json",
+            type: "post",
+            data: JSON.stringify(this.state),
+            success: novaListagem => {
+                console.log(this.state);
+                PubSub.publish('player-list-update', novaListagem);
+                this.setState({nome:''});
+            },
+            error: resposta => console.log(resposta)
+        });    
+    }
+
+    setNome(sender){
+        this.setState({nome: sender.target.value});
+    }
+}
